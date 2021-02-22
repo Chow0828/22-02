72c79bbb17f855110a74f7f747f5a3038c741693
Chow0828 <74063033+Chow0828@users.noreply.github.com>
2021-02-22 14:28:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calcularPreco(component){
+    if(component.val() < 0 || component.val() === ""){
+      component.val(0);
+    }
+
+    var preco = component.data('preco');
+    var quantidade = component.val();
+    var id = component.data('id');
+
+    var subtotal = preco * quantidade;
+
+    $('#sub'+id).text(subtotal.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"}));
+
+    var total = 
+        ($('#1').data('preco') * $('#1').val()) +
+        ($('#2').data('preco') * $('#2').val()) ;
+
+    $('#total').text(total.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"}));
+  }
